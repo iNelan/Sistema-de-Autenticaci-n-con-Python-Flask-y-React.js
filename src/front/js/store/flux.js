2a3f2ef24a1e34bbac68944ee64ab0bed069b0a9
iNelan <clpayala@gmail.com>
2022-06-30 08:57:48 +0000

    version2

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,54 +1,97 @@
-const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
+const getState = ({
+    getStore,
+    getActions,
+    setStore
+}) => {
+    return {
+        store: {
+            message: null,
+            demo: [{
+                    title: "FIRST",
+                    background: "white",
+                    initial: "white",
+                },
+                {
+                    title: "SECOND",
+                    background: "white",
+                    initial: "white",
+                },
+            ],
+        },
+        actions: {
+            // Use getActions to call a function within a fuction
+            exampleFunction: () => {
+                getActions().changeColor(0, "green");
+            },
 
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
-				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+            login: (email, password) => {
+                fetch(
+                        "https://3001-4geeksacade-reactflaskh-1qgtyf2qbj2.ws-eu47.gitpod.io/api/login", {
+                            method: "POST",
+                            body: JSON.stringify({
+                                email: email,
+                                password: password,
+                            }),
+                            headers: {
+                                "Content-Type": "application/json",
+                            },
+                        }
+                    )
+                    .then((response) => {
+                        if (response.status === 200) {
+                            setStore({
+                                auth: true,
+                            });
+                        }
+                        return response.json();
+                    })
+                    .then((data) => {
+                        console.log(data);
+                        localStorage.setItem("token", data.access_token);
+                    });
+            },
+            logout: () => {
+                localStorage.removeItem("token");
+                setStore({
+                    auth: false,
+                });
+            },
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+            getMessage: async () => {
+                try {
+                    // fetching data from the backend
+                    const resp = await fetch(
+                        process.env.BACKEND_URL +
+                        "/api/NOMBRE DE LA DEF OBJETO CREADO EN ROUTES"
+                    );
+                    const data = await resp.json();
+                    setStore({
+                        message: data.message,
+                    });
+                    // don't forget to return something, that is how the async resolves
+                    return data;
+                } catch (error) {
+                    console.log("Error loading message from backend", error);
+                }
+            },
+            changeColor: (index, color) => {
+                //get the store
+                const store = getStore();
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+                //we have to loop the entire demo array to look for the respective index
+                //and change its color
+                const demo = store.demo.map((elm, i) => {
+                    if (i === index) elm.background = color;
+                    return elm;
+                });
+
+                //reset the global store
+                setStore({
+                    demo: demo,
+                });
+            },
+        },
+    };
 };
 
 export default getState;
